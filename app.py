a3a78334fa326005b95dc93288dc600e26f2fbe7
Rubayet Mahmud <m.rubayet94@gmail.com>
2023-02-18 20:55:58 +0600

    route with add_url_rule function

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
 def index():
     return "This is a flask app."
 
 
-@app.route('/hello')
 def hello_world():
     return "Hello World!"
 
+
+app.add_url_rule('/', 'index', index)
+app.add_url_rule('/hello', 'hello', hello_world)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
