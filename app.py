bd30c74c85507c34756cdeffcec1a0bf725e0a5b
Rubayet Mahmud <m.rubayet94@gmail.com>
2023-02-18 21:00:14 +0600

    variable url route

--- app.py
+++ app.py
@@ -11,8 +11,13 @@ def hello_world():
     return "Hello World!"
 
 
+def hello_to(name):
+    return f"Hello, {name}!"
+
+
 app.add_url_rule('/', 'index', index)
 app.add_url_rule('/hello', 'hello', hello_world)
+app.add_url_rule('/hello/<name>', 'hello_to', hello_to)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
